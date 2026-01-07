dd5a9fa850b7e92529c938b2df1e5429ae81c2b9
CH0C0M1NT <rv2s7b7yzt@privaterelay.appleid.com>
2026-01-07 22:10:39 +0900

    Add migration and upgrade
    - migrate from CRA to Vite with pnpm
    - upgrade React to latest
    - update setup instructions for Vite + pnpm

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+root.render(<App />);
